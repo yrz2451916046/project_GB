be4b37b02c60e63bff27db7035c9d43fa6b1c2a7
yrz2451916046 <2451916046@qq.com>
2018-05-04 15:35:28 +0800

    登录

--- src/components/Back_stage_management/AdminLogin.vue
+++ src/components/Back_stage_management/AdminLogin.vue
@@ -1,58 +1,74 @@
 <template>
-	<div class="login-box">
-		<div class="login-logo">
-			<a href="../../index2.html"><b>Admin</b>LTE</a>
-		</div>
-		<!-- /.login-logo -->
-		<div class="login-box-body">
-			<p class="login-box-msg">Sign in to start your session</p>
-
-			<form action="../../index2.html" method="post">
-				<div class="form-group has-feedback">
-					<input type="email" class="form-control" placeholder="Email">
-					<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-				</div>
-				<div class="form-group has-feedback">
-					<input type="password" class="form-control" placeholder="Password">
-					<span class="glyphicon glyphicon-lock form-control-feedback"></span>
-				</div>
-				<div class="row">
-					<div class="col-xs-8">
-						<div class="checkbox icheck">
-							<label>
-								<input type="checkbox"> Remember Me
-							</label>
+	<!-- <div class="hold-transition login-page"> -->
+		<div class="login-box">
+			<!-- /.login-logo -->
+			<div class="card">
+				<div class="card-body login-card-body">
+					<div class="login-logo">
+						<a href="#"><b style="font-size:28px;">工保网业务后台管理系统</b></a>
+					</div>
+					<!-- <p class="login-box-msg">Sign in to start your session</p> -->
+					<form action="../../index2.html" method="post">
+						<div class="form-group has-feedback">
+							<input type="email" class="form-control" placeholder="请输入用户名称">
+							<!-- <span class="fa fa-envelope form-control-feedback"></span> -->
 						</div>
-					</div>
-					<!-- /.col -->
-					<div class="col-xs-4">
-						<button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-					</div>
-					<!-- /.col -->
+						<div class="form-group has-feedback">
+							<input type="password" class="form-control" placeholder="请输入登录密码">
+							<!-- <span class="fa fa-lock form-control-feedback"></span> -->
+						</div>
+						<div class="row">
+							<!-- <div class="col-8">
+								<div class="checkbox icheck">
+								<label>
+									<input type="checkbox"> Remember Me
+								</label>
+								</div>
+							</div> -->
+							<!-- /.col -->
+							<div class="col-4">
+								<button type="submit" class="btn btn-primary btn-block btn-flat" style="width:320px;border-radius:5px;">
+									登 录
+								</button>
+							</div>
+							<!-- /.col -->
+						</div>
+					</form>
+				<!-- <div class="social-auth-links text-center mb-3">
+					<p>- OR -</p>
+					<a href="#" class="btn btn-block btn-primary">
+						<i class="fa fa-facebook mr-2"></i> Sign in using Facebook
+					</a>
+					<a href="#" class="btn btn-block btn-danger">
+						<i class="fa fa-google-plus mr-2"></i> Sign in using Google+
+					</a>
+				</div> -->
+				<!-- /.social-auth-links -->
+
+				<!-- 忘记密码 -->
+				<!-- <p class="mb-1">
+					<a href="#">I forgot my password</a>
+				</p>
+				<p class="mb-0">
+					<a href="register.html" class="text-center">Register a new membership</a>
+				</p> -->
 				</div>
-			</form>
-
-			<div class="social-auth-links text-center">
-				<p>- OR -</p>
-				<a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-					Facebook</a>
-				<a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-					Google+</a>
+				<!-- /.login-card-body -->
 			</div>
-			<!-- /.social-auth-links -->
-
-			<a href="#">I forgot my password</a><br>
-			<a href="register.html" class="text-center">Register a new membership</a>
-
 		</div>
-		<!-- /.login-box-body -->
-	</div>
-	<!-- /.login-box -->
+		<!-- /.login-box -->
+	<!-- </div> -->
 </template>
 <script>
 	export default {
 	  name: 'adminLogin'
 	}
+	$(function(){
+		// console.log(window.location.pathname);
+		if(window.location.pathname == "/AdminLogin"){
+			$("body").css("background","#e9ecef")
+		}
+	})
 </script>
 <style>
 	
